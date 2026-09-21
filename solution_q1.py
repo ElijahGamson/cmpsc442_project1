5272814a81507f3fa4c0f4078b418b5672da3605
Elijah Gamson <egamson50@gmail.com>
2026-09-21 14:25:29 -0400

    Created q1 file

---
+++ solution_q1.py
@@ -0,0 +1,4 @@
+"""Implement DFS algorithm that works with starting state that is read in from an input file called
+input.txt. The format of the state should be the same as what we mention above.
+• Output: path of states returned, total cost (where cost is defined in terms of number of
+actions),and number of node expansions."""
