1c34146fe1292e2e62ae185539aed7932950b14d
Elijah Gamson <egamson50@gmail.com>
2026-09-22 13:44:23 -0400

    Added instructions for q1

--- solution_q1.py
+++ solution_q1.py
@@ -2,3 +2,35 @@
 input.txt. The format of the state should be the same as what we mention above.
 • Output: path of states returned, total cost (where cost is defined in terms of number of
 actions),and number of node expansions."""
+
+"""The program must read from input.txt, which specifies the initial state of the puzzle
+• The state will be represented as: M_left,C_left,M_right,C_right,Boat (Ex: 3, 3, 0, 0, L)"""
+
+def depth_fs():
+    """
+    • Implement DFS algorithm that works with starting state that is read in from an input file called
+        input.txt. The format of the state should be the same as what we mention above.
+    • Output: path of states returned, total cost (where cost is defined in terms of number of
+        actions),and number of node expansions.
+        
+    Example Terminal Output:
+    The solution of Q1.1.a (DFS) is:
+    Solution Path: <Path>
+    Total cost = <number>
+    Number of node expansions = <number>\n\n"""
+    pass
+
+def breadth_fs():
+    """
+    • Implement a BFS that works with starting state that is read in from an input file called input.txt.
+        The format of the state should be the same as what we mention above.
+    • Output: path of states returned, total cost (where cost is defined in terms of number of actions),
+        and number of node expansions.
+
+    Example Terminal Output:
+    The solution of Q1.1.b (BFS) is:
+    Solution Path: <Path>
+    Total cost = <number>
+    Number of node expansions = <number>
+    """
+    pass
