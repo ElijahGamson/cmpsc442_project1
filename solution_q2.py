9402b5add484a2a593ef518f48333589acb62057
Elijah Gamson <egamson50@gmail.com>
2026-09-24 12:32:09 -0400

    Added UCS Model A

--- solution_q2.py
+++ solution_q2.py
@@ -8,7 +8,121 @@ def uniform_cost_a():
     • Move {M, M} → cost 4
     • Move {C, C} → cost 2
     • Move {M, C} → cost 3"""
-    pass
+    with open("input.txt", "r") as file:
+            contents = file.read()
+    m_left, c_left, m_right, c_right, boat = contents.split(",")
+    initial_state = [
+        int(m_left.strip()), 
+        int(c_left.strip()), 
+        int(m_right.strip()),
+        int(c_right.strip()),
+        boat.strip()
+    ]
+
+    # set of possible actions with changes to numbers of missionaries/cannibals per side
+    #! IS the L or R dictating what side the boat is currently on before the move (the two letters that follow)
+    actions = {
+        "LCC": [0, -2, 0, 2],
+        "LC":  [0, -1, 0, 1],
+        "LCM": [-1, -1, 1, 1],
+        "LM":  [-1, 0, 1, 0],
+        "LMM": [-2, 0, 2, 0],
+
+        "RCC": [0, 2, 0, -2],
+        "RC":  [0, 1, 0, -1],
+        "RCM": [1, 1, -1, -1],
+        "RM":  [1, 0, -1, 0],
+        "RMM": [2, 0, -2, 0]
+    }
+
+    # UCS structure: each queue entry has tuple (total cost, current state, path to current state)
+
+    queue = []
+
+    queue.append((0, initial_state, [initial_state]))
+
+    # stores cheapest cost found for each state so far
+    best_cost = {}
+    best_cost[tuple(initial_state)] = 0
+
+    expansions = 0
+
+    # implement UCS
+    while len(queue) > 0:
+
+        # main difference from BFS: find node with lowest total cost
+
+        lowest_index = 0
+        for i in range(1, len(queue)):
+            if queue[i][0] < queue[lowest_index][0]:
+                lowest_index = i
+
+        current_cost, current_state, current_path = queue.pop(lowest_index)
+
+        # ignore this node if a cheaper route to this state has been found previously
+
+        if current_cost > best_cost[tuple(current_state)]:
+            continue
+
+        #check for success condition (all on right bank)
+        if current_state[0] == 0 and current_state[1] == 0:
+            print("The solution of Q2.1 (UCS, cost model A) is:")
+            print("Solution Path:")
+
+            for state in current_path:
+                print(state)
+
+            print("Total cost = ", current_cost)
+            print("Number of node exansions = ", expansions)
+
+            return current_path
+
+        # if goal not reached, expand this node
+        expansions += 1
+
+        # generate possible next states (nodes) from action set
+        for key in actions:
+            # only need to look at actions from current boat side
+            if key[0] == current_state[4]:
+
+                new_state = []
+                # calculates new values from current action
+                for j in range(4):
+                    new_state.append(current_state[j] + actions[key][j])
+
+                # move boat
+                if current_state[4] == "L":
+                    new_state.append("R")
+                else:
+                    new_state.append("L")
+
+                #check new state for validity
+
+                if valid_state(new_state):
+                    # calculate cost for model A ({M, M} = 4, {C, C} = 2, {M, C} = 3)
+                    m_moved = abs(actions[key][0]) #Get how many of each group moved
+                    c_moved = abs(actions[key][1])
+
+                    action_cost = 2 * m_moved + c_moved
+
+                    new_cost = current_cost + action_cost
+                    new_state_tuple = tuple(new_state)
+
+                    # ensure this state is not visited already and no cheaper path has been found
+                    if new_state_tuple not in best_cost or new_cost < best_cost[new_state_tuple]:
+                        best_cost[new_state_tuple] = new_cost
+
+                        # add action/state to path 
+                        new_path = current_path + [new_state]
+                        #add the new tuple to the queue
+                        queue.append((new_cost, new_state, new_path))
+
+    # empty queue -> no solution
+    
+    print("The solution of Q2.1 (UCS, cost model A) is:")
+    print("No solution found.")
+
+    return None
 
 def uniform_cost_b():
     """
